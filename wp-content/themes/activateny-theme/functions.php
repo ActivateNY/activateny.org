73d1ca56d5ff6341bc454a1614eb97846668802c
Pea <patricia@nycga.net>
2013-04-06 19:36:03 -0400

    update styles

---
+++ wp-content/themes/activateny-theme/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function theme_styles()  
+{ 
+  // Register the style like this for a theme:  
+  // (First the unique name for the style (custom-style) then the src, 
+  // then dependencies and ver no. and media type)
+  wp_register_style( 'custom-style', 
+    get_stylesheet_directory() . '/css/custom-style.css', 
+    array(), 
+    '20120208', 
+    'all' );
+
+  // enqueing:
+  wp_enqueue_style( 'custom-style' );
+}
+add_action('wp_enqueue_scripts', 'theme_styles');
+
+?>
